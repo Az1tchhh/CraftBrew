9ff4a3852c0bd036a3ae4fe50f46dd8ea6c1c280
kud-aa <akudaybergenov@miem.hse.ru>
2023-04-09 20:58:36 +0600

    changed to buster image because of psycopg need to be compiled on apline

--- backend/example/urls.py
+++ backend/example/urls.py
@@ -17,5 +17,6 @@ from django.urls import include, re_path
 from django.contrib import admin
 
 urlpatterns = [
-   re_path(r'^admin/', admin.site.urls),
+    re_path(r'^api/', include('api.urls')),
+    re_path(r'^admin/', admin.site.urls),
 ]
