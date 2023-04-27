b3189d946b013f98ba60ac28c08078d13cbca4a8
RepDD <98698562+Az1tchhh@users.noreply.github.com>
2023-04-28 00:25:22 +0600

    modified

--- frontend/src/app/authentication/authentication.component.ts
+++ frontend/src/app/authentication/authentication.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
-
+import {Component, OnInit} from '@angular/core';
+import {LoginService} from "../login.service";
+import {AppComponent} from "../app.component";
+import {Router} from "@angular/router";
 @Component({
   selector: 'app-authentication',
   templateUrl: './authentication.component.html',
   styleUrls: ['./authentication.component.css']
 })
-export class AuthenticationComponent {
+export class AuthenticationComponent{
+  username!:string;
+  password!:string;
 
+
+  constructor(private loginService: LoginService, private router: Router) {
+  }
+  login(){
+    this.loginService.login(this.username, this.password).subscribe((data)=>{
+      console.log(data);
+      localStorage.setItem('token', data.access);
+      AppComponent.isLogged = true;
+      this.router.navigate([''])
+    })
+  }
 }
--- frontend/src/app/category/category.component.ts
+++ frontend/src/app/category/category.component.ts
@@ -1,21 +1,33 @@
-import { Component } from '@angular/core';
-import {Product, products} from "../products";
-
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Product} from "../products";
+import {CategoriesService} from "../categories.service";
+import {Category} from "../categories";
+import {ProductsService} from "../products.service";
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
   styleUrls: ['./category.component.css']
 })
-export class CategoryComponent {
-  products = products
-
-  getCategory() {
-    const categories = new Set<string>();
-    this.products.forEach( product => {
-      categories.add(product.category);
-    });
-    return Array.from(categories);
+export class CategoryComponent implements OnInit{
+  @Output() productsCategorised = new EventEmitter<Product[]>();
+  categories!:Category[];
+  constructor(private categoryService: CategoriesService, private productService: ProductsService) {
   }
-
-
+  ngOnInit() {
+    this.categoryService.getCategories().subscribe((data) => {
+      console.log(data);
+      this.categories = data;
+    })
+  }
+  setList(id: number){
+    if (id == 0){
+      this.productService.getProducts().subscribe((data)=>{
+        this.productsCategorised.emit(data);
+      })
+      return;
+    }
+    this.categoryService.getProductsByCategory(id).subscribe((data)=>{
+      this.productsCategorised.emit(data);
+    })
+  }
 }
